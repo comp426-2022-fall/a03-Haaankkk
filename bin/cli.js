05e00ca008814003362a66c89f93af294d20fa8a
Haaankkk <hepfhank@sina.com>
2022-11-29 01:26:35 -0500

    update

--- bin/cli.js
+++ bin/cli.js
@@ -7,15 +7,21 @@ const args = minimist(process.argv.slice(2));
 
 //Initialization
 if (args.sides) {
-    var side = args.side;
+    var side = args.sides;
+} else {
+    var side = 0;
 }
 
 if (args.dice) {
     var dice = args.dice;
+} else {
+    var dice = 0;
 }
 
 if (args.rolls) {
     var rolls = args.rolls;
+} else {
+    var rolls = 0;
 }
 
 //Output
